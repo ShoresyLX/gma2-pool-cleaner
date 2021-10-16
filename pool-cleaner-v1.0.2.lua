40791d5404889a5da8025a0205df12761065c827
Calea-Designs <92617138+Calea-Designs@users.noreply.github.com>
2021-10-15 22:09:15 -0700

    pool-cleaner-v1.0.3
    Final proofread, fixed spacing throughout file

--- pool-cleaner-v1.0.2.lua
+++ pool-cleaner-v1.0.2.lua
@@ -23,66 +23,68 @@ local get_name = gma.show.getobj.name
 
 -- functions
 function match(a, b) -- matches strings, returns false if they are not strings
-  if type(a) == 'string' and type(b) == 'string' then
-    if string.find(string.lower(a), string.lower(b)) ~= nil and
-       string.len(a) == string.len(b) then
-      return true
-    else return false
+    if type(a) == 'string' and type(b) == 'string' then
+        if string.find(string.lower(a), string.lower(b)) ~= nil and
+            string.len(a) == string.len(b) then
+        return true
+        else return false
+        end
+    else return false;
     end
-  else return false;
-  end
 end
 
 function strip(text, n) -- strips id numbers from pool object names
     if type(text) == 'string' then
-      n = tostring(n)
-      text = string.gsub(text, n, '')
+        n = tostring(n)
+        text = string.gsub(text, n, '')
     end
-  return text
+return text
 end
 
 --plugin start
 function Start ()
-  ma_print('POOL CLEANER ACTIVATED')
-  local pool = {} -- our "scratch paper"
-  local load_id = start_id; -- isolate start_id for safety
+    ma_print('POOL CLEANER ACTIVATED')
+    local pool = {} -- our "scratch paper"
+    local load_id = start_id; -- isolate start_id for safety
 
-  -- loading phase
+    -- loading phase
 
-  ma_print("LOADING POOL OBJECTS")
-  while load_id <= finish_id do -- loads required pool into a table
-  	local handle = get_handle(pool_type..' '..load_id);
-  	if handle ~= nil then
-       local a = get_name(handle)
-  	   pool[load_id] = a; -- assigns name to working pool with og pool position as id
-  	end
-  	load_id = load_id + 1;
-  end
-  ma_print('LOAD SUCCESSFUL, BEGINNING DELETION')
-
-  -- comparison and deletion phase
-
-  local comp_id = start_id; -- isolate start_id for safety
-  local killcount = 0 -- counter for deleted objects
-  while comp_id <= finish_id do -- main while loop
-  	local x = comp_id + 1;
-  	local object_name = pool[comp_id];
-    if object_name ~= nil and get_handle(pool_type..' '..comp_id) ~= nil then
-  	   while x <= finish_id do -- steps thru each subsequent object to see if there is a match
-  			if pool[x] ~= nil and get_handle(pool_type..' '..x) ~= nil then
-  				if match(strip(pool[x], x), strip(object_name, comp_id)) == true then
-              ma_print('FOUND MATCH: {'..pool[comp_id]..', '..pool[x]..'}')
-              ma_print('DELETING: '..pool[x])
-              ma_cmd('Delete '..pool_type..' '..x)
-              killcount = killcount + 1
-  				end
-  			end
-  		 x = x + 1
-  	   end
+    ma_print("LOADING POOL OBJECTS")
+    
+    while load_id <= finish_id do -- loads required pool into a table
+  	    local handle = get_handle(pool_type..' '..load_id);
+        if handle ~= nil then
+                local a = get_name(handle)
+  	            pool[load_id] = a; -- assigns name to working pool with og pool position as id
+        end
+  	    load_id = load_id + 1;
     end
-  	comp_id = comp_id + 1
-  end
-  ma_print('DELETED '..killcount..' '..string.upper(pool_type)..'S')
-  ma_print('END OF PLUGIN')
+    
+    ma_print('LOAD SUCCESSFUL, BEGINNING DELETION')
+
+    -- comparison and deletion phase
+
+    local comp_id = start_id; -- isolate start_id for safety
+    local killcount = 0 -- counter for deleted objects
+    while comp_id <= finish_id do -- main while loop
+        local x = comp_id + 1;
+  	    local object_name = pool[comp_id];
+        if object_name ~= nil and get_handle(pool_type..' '..comp_id) ~= nil then
+  	        while x <= finish_id do -- steps thru each subsequent object to see if there is a match
+  			    if pool[x] ~= nil and get_handle(pool_type..' '..x) ~= nil then
+  				    if match(strip(pool[x], x), strip(object_name, comp_id)) == true then
+                        ma_print('FOUND MATCH: {'..pool[comp_id]..', '..pool[x]..'}')
+                        ma_print('DELETING: '..pool[x])
+                        ma_cmd('Delete '..pool_type..' '..x)
+                        killcount = killcount + 1
+  				    end
+  			    end
+                x = x + 1
+            end
+        end
+        comp_id = comp_id + 1
+    end
+    ma_print('DELETED '..killcount..' '..string.upper(pool_type)..'S')
+    ma_print('END OF PLUGIN')
 end
 return Start
